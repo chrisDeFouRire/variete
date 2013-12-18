db1416730701d8c4ce7dda1d50a8d281b30bd98d
Chris Hartwig <chris@chris-hartwig.com>
2013-12-18 10:50:09 +0100

    Added tests to check for exceptions

--- test/test.js
+++ test/test.js
@@ -27,5 +27,19 @@ describe('variete', function () {
             var result = variete.parse('always {alt1|alt2} {alt3|alt4}');
             result.should.match(/^always (alt1|alt2) (alt3|alt4)$/);
         });
+        it('should have a working example in README.md', function() {
+            var desc = variete.parse('This {amazing|extraordinary|nice} module {will help me|already helps me} when I {need|have to} vary my copy.');
+            desc.should.match(/^This (amazing|extraordinary|nice) module (will help me|already helps me) when I (need|have to) vary my copy.$/);
+        });
+        it('shoud throw exceptions with empty text', function() {
+            var result = function() {return variete.parse('');};
+            result.should.throw();
+        });
+        it('shoud throw exceptions when using {} in your text', function() {
+            var result = function() {return variete.parse('this should { not work');};
+            result.should.throw();
+            result = function() {return variete.parse('but this should } work');};
+            result.should.not.throw();
+        });
     });
 });
